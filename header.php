825403f374fde3937c2b8efd6668550fd255d8cd
Omar Ochoa <omar@ochoawebdesign.com>
2023-02-20 18:48:07 -0800

    Header: Adds logo link back to home

--- header.php
+++ header.php
@@ -5,3 +5,12 @@
   <?php wp_head(); ?>
   <title><?php the_title(); ?></title>
 </head>
+<header>
+  <div><a href="<?php bloginfo('home'); ?>"><p>Theme<strong>One</srong></p></a></div>
+  <ul>
+    <li><a href="#">Page One</a></li>
+    <li><a href="#">Page Two</a></li>
+    <li><a href="#">Page Three</a></li>
+    <li><a href="#">Page Four</a></li>
+  </ul>
+</header>
