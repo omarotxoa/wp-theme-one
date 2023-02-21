77532949f39cf5e6e84ed8fe49777368dca5a6ed
Omar Ochoa <omar@ochoawebdesign.com>
2023-02-20 20:42:00 -0800

    Template: Page.php and Index.php now have a <main> element

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
-<h2>Posts:</h2>
-<ul>
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-  <?php endwhile; endif; ?>
-</ul>
+<main>
+  <h2>Posts:</h2>
+  <ul>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php endwhile; endif; ?>
+  </ul>
+</main>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-  <h1>Page: <?php the_title(); ?></h1>
-  <p><?php the_content(); ?></p>
-<?php endwhile; endif; ?>
+<main>
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h1>Page: <?php the_title(); ?></h1>
+    <p><?php the_content(); ?></p>
+  <?php endwhile; endif; ?>
+</main>
 
 <?php get_footer(); ?>
